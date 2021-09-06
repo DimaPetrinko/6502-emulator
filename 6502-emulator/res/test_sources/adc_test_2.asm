a6836838d68ab05886a697228fce8b2d8c09efb1
Dmytrii Petrinko <petrinko6@gmail.com>
2021-09-06 21:43:51 +0300

    Made comments pretty

--- 6502-emulator/res/test_sources/adc_test_2.asm
+++ 6502-emulator/res/test_sources/adc_test_2.asm
@@ -1,26 +1,26 @@
-  lda #$ff ; resets v, n, a. sets c to 1
-  adc #$01
+	lda #$ff			; resets v, n, a. sets c to 1
+	adc #$01
 
-  lda #127
-  adc #1 ; 127 + 1 + c = 129/-127 ; c = 0, v = 1, n = 1
-
-
-  lda #$ff ; resets v, n, a. sets c to 1
-  adc #$01
-
-  lda #126
-  adc #1 ; 126 + 1 + c = 128/-128 ; c = 0, v = 1, n = 1
+	lda #127			; 127 + 1 + c = 129/-127
+	adc #1				; c = 0, v = 1, n = 1
 
 
-  lda #$ff ; resets v, n, a. sets c to 1
-  adc #$01
+	lda #$ff			; resets v, n, a. sets c to 1
+	adc #$01
 
-  lda #128
-  adc #255 ; 128(-128) + 255(-1) + c = 384 -> 128/-128 ; c = 1, v = 0, n = 1
+	lda #126			; 126 + 1 + c = 128/-128
+	adc #1				; c = 0, v = 1, n = 1
 
 
-  lda #$ff ; resets v, n, a. sets c to 1
-  adc #$01
+	lda #$ff			; resets v, n, a. sets c to 1
+	adc #$01
 
-  lda #128
-  adc #254 ; 128(-128) + 254(-2) + c = 383 -> 127 ; c = 1, v = 1, n = 0
+	lda #128			; 128(-128) + 255(-1) + c
+	adc #255			; = 384 -> 128/-128 ; c = 1, v = 0, n = 1
+
+
+	lda #$ff			; resets v, n, a. sets c to 1
+	adc #$01
+
+	lda #128			; 128(-128) + 254(-2) + c 
+	adc #254			; = 383 -> 127 ; c = 1, v = 1, n = 0
